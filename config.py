21bc7e458b082cfed409f725545f3ebccce64d76
shuabhkumarmishra412-create <shuabhkumarmishra412@gmail.com>
2026-04-25 13:53:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,10 @@ BOT_USERNAME = getenv("BOT_USERNAME", "ll_YOUR_MUSIC_llBOT")
 BOT_NAME = getenv("BOT_NAME", "˹ 𝐀ʀꜱᴇᴇɴᴀ ꭙ 𝐌ᴜꜱɪᴄ ♪˼ [ ɴᴏ ᴀᴅs ]")
 ASSUSERNAME = getenv("ASSUSERNAME", "YOUR_MADARA_BRO")
 
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_H4kCLtcb03xrPVBA55khulZgqMKeT_8f' ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+
 # MongoDB
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
